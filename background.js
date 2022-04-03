dbdc76ae4c03b9640c069e9fb2710e10a2ad5773
laserroger <laserroger@live.com>
2022-04-03 00:58:52 -0400

    supporting multiple user with settings

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function({url: currentUrl}) {
-  if (!currentUrl.match('authuser')) {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      const tab = tabs[0];
-      const separator = currentUrl.match("\\?") ? "&" : "?";
-      const destination = `${currentUrl}${separator}authuser=1`;
-      console.info(`Redirected from ${currentUrl} to ${destination}`);
-      chrome.tabs.update(tab.id, {url: destination});
-    });
-  }
+chrome.webNavigation.onBeforeNavigate.addListener(function({url: currentUrl}) {	
+	if (!currentUrl.match('authuser')) {
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			const tab = tabs[0];
+			const separator = currentUrl.match("\\?") ? "&" : "?";
+      			var destination = `${currentUrl}${separator}`;
+			chrome.storage.local.get({'map': '0'}, function(result) {
+				if (parseInt(result.map)){
+					destination = `${currentUrl}${separator}authuser=${result.map}`;
+					console.info(`Redirected from ${currentUrl} to ${destination}`);
+					chrome.tabs.update(tab.id, {url: destination});
+				}
+			});
+		});
+	}
 }, {url: [{ urlMatches: 'google.com\/maps' }, { hostEquals: 'map.google.com' }, { hostEquals: 'maps.google.com' }]
 });
